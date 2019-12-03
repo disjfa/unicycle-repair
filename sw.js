a1ef19d8992a25bc28b7d3564257b84416de7bfc
disjfa <disjfa@disjfa.nl>
2019-12-03 23:13:55 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.4bef53a5cd9fc22d9b81275aa458285e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.d139de3a7d81139a05a46e1225cb3d7c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
