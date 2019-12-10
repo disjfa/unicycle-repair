0ed8d8241b7f8e71d4d5f01dca7b1816c68bb119
disjfa <disjfa@disjfa.nl>
2019-12-10 22:48:56 +0000

    Updates

---
+++ precache-manifest.3e2c702efdb32f317fc92c6f7497fa09.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b050eedfed317e655ec43dc8a5c90695",
+    "url": "css/defaults.css"
+  },
+  {
+    "revision": "38b156b2276bf9cdef9a860d0add6e38",
+    "url": "css/embed.css"
+  },
+  {
+    "revision": "4a2517375598e6640ee062e31695b44c",
+    "url": "css/header.css"
+  },
+  {
+    "revision": "b067db2f2cdc4c328191a5e2786280c5",
+    "url": "css/main.css"
+  },
+  {
+    "revision": "fdbda993394414526f447bd67627e4d4",
+    "url": "css/responsive.css"
+  },
+  {
+    "revision": "4e36c5b24cf19af9b1c12edf162d4519",
+    "url": "css/screens.css"
+  },
+  {
+    "revision": "19d558614d95f89418485ceafdb2a962",
+    "url": "css/svg-with-js.css"
+  },
+  {
+    "revision": "9445d710222f0a6ee37cabfff4033274",
+    "url": "css/tabs.css"
+  },
+  {
+    "revision": "708ca715056a9be0583581172c06317f",
+    "url": "css/variables.css"
+  },
+  {
+    "revision": "33b667885b5db47c6d7263427a94e6be",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "c9bb4e7f6f9a0f7c3b57752a02719cac",
+    "url": "img/android-chrome-1024x1024.png"
+  },
+  {
+    "revision": "166c7561ea7762fc10e389f15f0965d5",
+    "url": "img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "012a5d7555a3031a492cee3d10364403",
+    "url": "img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "550c1549aa91b6c06ba79495402d36d3",
+    "url": "img/screen-desktop.png"
+  },
+  {
+    "revision": "aef1521c5c478b8247f24e304536766f",
+    "url": "img/screen-mobile.png"
+  },
+  {
+    "revision": "ba79fbf249d5559b0218b9a1310c47c0",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b28ed84c02e90d72ea4",
+    "url": "main.js"
+  },
+  {
+    "revision": "bde50bdd9423b20b7207d4d3e1b8537d",
+    "url": "manifest.json"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.ced79efa462064e97a71c1d5885f84c9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.3e2c702efdb32f317fc92c6f7497fa09.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
